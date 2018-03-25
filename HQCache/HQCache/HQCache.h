3503c5aa685d547ffb78878b35f412bd6f821a5b
CanyFrog <huangcong@iMac.lan>
2018-03-26 07:41:19 +0800

    Create a cache branch based on existing code.

---
+++ HQCache/HQCache/HQCache.h
@@ -0,0 +1,20 @@
+//
+//  HQCache.h
+//  HQCache
+//
+//  Created by qihuang on 2018/3/26.
+//  Copyright © 2018年 com.personal.HQ. All rights reserved.
+//
+// reference to YYCache https://github.com/ibireme/YYCache
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HQCache.
+FOUNDATION_EXPORT double HQCacheVersionNumber;
+
+//! Project version string for HQCache.
+FOUNDATION_EXPORT const unsigned char HQCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HQCache/PublicHeader.h>
+
+
