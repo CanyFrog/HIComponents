8eb4224c208b860c6dd630196d58863835100f4d
Magee Huang <magee.huang@blackboard.com>
2018-04-02 13:36:05 +0800

    Add object lock and pthread lock

--- HQFoundation/HQFoundation/Lock/HQLock.swift
+++ HQFoundation/HQFoundation/Lock/HQLock.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+// MARK: - Object lock
+public struct HQObjectLock {
+    
+    func synchronized(_ lock: AnyObject, closure: () -> ()) {
+        objc_sync_enter(lock)
+        defer { objc_sync_exit(lock) }
+        closure()
+    }
+    
+    func synchronized<T>(_ lock: AnyObject, closure: () throws -> T) rethrows -> T {
+        objc_sync_enter(lock)
+        defer { objc_sync_exit(lock) }
+        return try closure()
+    }
+}
+
+
+
+
+// MARK: - Pthread lock
+class HQMutexLock {
+    private let _mutex = UnsafeMutablePointer<pthread_mutex_t>.allocate(capacity: 1)
+    init() {
+        pthread_mutex_init(_mutex, nil)
+    }
+    
+    static func autoLock(_ mutex: HQMutexLock, closure: () -> Void) {
+        mutex.lock()
+        defer { mutex.unlock() }
+        closure()
+    }
+    
+    static func autoLock<T>(_ mutex: HQMutexLock, closure: () throws -> T) rethrows -> T {
+        mutex.lock()
+        defer { mutex.unlock() }
+        return try closure()
+    }
+    
+    @discardableResult
+    func lock() -> Int32 {
+        return pthread_mutex_lock(_mutex)
+    }
+    
+    @discardableResult
+    func unlock() -> Int32 {
+        return pthread_mutex_unlock(_mutex)
+    }
+    
+    @discardableResult
+    func tryLock() -> Int32 {
+        return pthread_mutex_trylock(_mutex)
+    }
+    
+    deinit {
+        pthread_mutex_destroy(_mutex)
+        _mutex.deallocate(capacity: 1)
+    }
+}
