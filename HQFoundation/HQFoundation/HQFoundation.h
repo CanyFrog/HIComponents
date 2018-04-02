69e45e1fba35bde22dda32fd6867338e8acc935b
Magee Huang <magee.huang@blackboard.com>
2018-04-02 13:32:11 +0800

    Init Foundation module

---
+++ HQFoundation/HQFoundation/HQFoundation.h
@@ -0,0 +1,19 @@
+//
+//  HQFoundation.h
+//  HQFoundation
+//
+//  Created by Magee Huang on 4/2/18.
+//  Copyright © 2018 HQ.Personal.modules. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HQFoundation.
+FOUNDATION_EXPORT double HQFoundationVersionNumber;
+
+//! Project version string for HQFoundation.
+FOUNDATION_EXPORT const unsigned char HQFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HQFoundation/PublicHeader.h>
+
+
---
+++ HQFoundation/HQFoundation/Lock/HQLock.swift
@@ -0,0 +1,9 @@
+//
+//  HQLock.swift
+//  HQFoundation
+//
+//  Created by Magee Huang on 4/2/18.
+//  Copyright © 2018 HQ.Personal.modules. All rights reserved.
+//
+
+import Foundation
