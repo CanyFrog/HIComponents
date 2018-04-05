fa0dca04e8c90e8d97980bc3d977e4fa2b330d86
honqi <honqi3014@gmail.com>
2018-04-05 09:50:51 +0800

    Add lock

--- HQFoundation/HQFoundation/Lock/HQLock.swift
+++ HQFoundation/HQFoundation/Lock/HQLock.swift
@@ -6,18 +6,46 @@
 //  Copyright © 2018 HQ.Personal.modules. All rights reserved.
 //
 
-import Foundation
+public struct HQDispatchLock {
+    public static func semaphore(_ lock: DispatchSemaphore, closure: () -> Void) {
+        let _ = lock.wait(timeout: .distantFuture)
+        defer { lock.signal() }
+        closure()
+    }
+    
+    @discardableResult
+    public static func semaphore<T>(_ lock: DispatchSemaphore, closure: () -> T) -> T {
+        let _ = lock.wait(timeout: .distantFuture)
+        defer { lock.signal() }
+        return closure()
+    }
+    
+    @discardableResult
+    public static func semaphore<T>(_ lock: DispatchSemaphore, closure: () throws -> T) rethrows -> T {
+        let _ = lock.wait(timeout: .distantFuture)
+        defer { lock.signal() }
+        return try closure()
+    }
+}
+
 
 // MARK: - Object lock
 public struct HQObjectLock {
-    
-    func synchronized(_ lock: AnyObject, closure: () -> ()) {
+    public static func synchronized(_ lock: AnyObject, closure: () -> ()) {
         objc_sync_enter(lock)
         defer { objc_sync_exit(lock) }
         closure()
     }
     
-    func synchronized<T>(_ lock: AnyObject, closure: () throws -> T) rethrows -> T {
+    @discardableResult
+    public static func synchronized<T>(_ lock: AnyObject, closure: () -> T) -> T {
+        objc_sync_enter(lock)
+        defer { objc_sync_exit(lock) }
+        return closure()
+    }
+    
+    @discardableResult
+    public static func synchronized<T>(_ lock: AnyObject, closure: () throws -> T) rethrows -> T {
         objc_sync_enter(lock)
         defer { objc_sync_exit(lock) }
         return try closure()
@@ -28,41 +56,41 @@ public struct HQObjectLock {
 
 
 // MARK: - Pthread lock
-class HQMutexLock {
+public final class HQMutexLock {
     private let _mutex = UnsafeMutablePointer<pthread_mutex_t>.allocate(capacity: 1)
-    init() {
+    public init() {
         pthread_mutex_init(_mutex, nil)
     }
     
-    static func autoLock(_ mutex: HQMutexLock, closure: () -> Void) {
+    public static func autoLock(_ mutex: HQMutexLock, closure: () -> Void) {
         mutex.lock()
         defer { mutex.unlock() }
         closure()
     }
     
-    static func autoLock<T>(_ mutex: HQMutexLock, closure: () throws -> T) rethrows -> T {
+    public static func autoLock<T>(_ mutex: HQMutexLock, closure: () throws -> T) rethrows -> T {
         mutex.lock()
         defer { mutex.unlock() }
         return try closure()
     }
     
     @discardableResult
-    func lock() -> Int32 {
+    public func lock() -> Int32 {
         return pthread_mutex_lock(_mutex)
     }
     
     @discardableResult
-    func unlock() -> Int32 {
+    public func unlock() -> Int32 {
         return pthread_mutex_unlock(_mutex)
     }
     
     @discardableResult
-    func tryLock() -> Int32 {
+    public func tryLock() -> Int32 {
         return pthread_mutex_trylock(_mutex)
     }
     
     deinit {
         pthread_mutex_destroy(_mutex)
-        _mutex.deallocate(capacity: 1)
+        _mutex.deallocate()
     }
 }
