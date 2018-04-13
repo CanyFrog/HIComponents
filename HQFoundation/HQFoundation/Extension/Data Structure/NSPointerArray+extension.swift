eaead51c9e87dc4599acc09bc2bba672f5f0aea9
honqi <honqi3014@gmail.com>
2018-04-13 08:25:27 +0800

    Add pointer array extension

---
+++ HQFoundation/HQFoundation/Extension/Data Structure/NSPointerArray+extension.swift
@@ -0,0 +1,41 @@
+//
+//  NSPointerArray+extension.swift
+//  HQFoundation
+//
+//  Created by qihuang on 2018/4/13.
+//  Copyright © 2018年 HQ.Personal.modules. All rights reserved.
+//
+
+public extension NSPointerArray {
+    public func addObject(_ object: AnyObject?) {
+        guard let strongObject = object else { return }
+        
+        let pointer = Unmanaged.passUnretained(strongObject).toOpaque()
+        addPointer(pointer)
+    }
+    
+    public func insertObject(_ object: AnyObject?, at index: Int) {
+        guard index < count, let strongObject = object else { return }
+        
+        let pointer = Unmanaged.passUnretained(strongObject).toOpaque()
+        insertPointer(pointer, at: index)
+    }
+    
+    public func replaceObject(at index: Int, withObject object: AnyObject?) {
+        guard index < count, let strongObject = object else { return }
+        
+        let pointer = Unmanaged.passUnretained(strongObject).toOpaque()
+        replacePointer(at: index, withPointer: pointer)
+    }
+    
+    public func object(at index: Int) -> AnyObject? {
+        guard index < count, let pointer = self.pointer(at: index) else { return nil }
+        return Unmanaged<AnyObject>.fromOpaque(pointer).takeUnretainedValue()
+    }
+    
+    public func removeObject(at index: Int) {
+        guard index < count else { return }
+        
+        removePointer(at: index)
+    }
+}
