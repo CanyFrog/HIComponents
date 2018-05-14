fa2e167f8cd7598b833be8901a32410039616596
Magee Huang <magee.huang@blackboard.com>
2018-05-14 16:52:05 +0800

    Add name space

--- HQFoundation/HQFoundation/Runtime/Runtime.swift
+++ HQFoundation/HQFoundation/Runtime/Runtime.swift
@@ -1,16 +1,17 @@
 //
-//  HQRuntime.swift
+//  Runtime.swift
 //  HQFoundation
 //
 //  Created by Qi on 2018/4/19.
 //  Copyright © 2018年 HQ.Personal.modules. All rights reserved.
 //
 
-public struct HQRuntime {
+public struct Runtime {
     
     /// Gets the Obj-C reference for the instance object within the UIView extension.
     /// If nil, initializer object and associated.
-    @discardableResult public static func AssociatedObject<T: Any>(base: Any, key: UnsafePointer<UInt8>, initializer: () -> T, policy: objc_AssociationPolicy = .OBJC_ASSOCIATION_RETAIN_NONATOMIC) -> T {
+    @discardableResult
+    public static func AssociatedObject<T: Any>(base: Any, key: UnsafePointer<UInt8>, initializer: () -> T, policy: objc_AssociationPolicy = .OBJC_ASSOCIATION_RETAIN_NONATOMIC) -> T {
         if let v = objc_getAssociatedObject(base, key) as? T { return v }
         
         let v = initializer()
