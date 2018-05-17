c93fe7045232170b87a862cc57d25e502545e81a
Magee Huang <magee.huang@blackboard.com>
2018-05-17 15:50:41 +0800

    Add demo

---
+++ HQRouter/HQRouterDemo/DemoComponent.swift
@@ -0,0 +1,37 @@
+//
+//  DemoComponent.swift
+//  HQRouterDemo
+//
+//  Created by Magee Huang on 5/17/18.
+//  Copyright © 2018 HQ.components.router. All rights reserved.
+//
+
+import HQRouter
+
+class DemoDataProvider: DataProvider {
+    
+}
+
+class DemoComponent: Component {
+    var uid: String!
+    
+    var urlComponent: RouterURLComponent!
+    
+    var router: Router?
+    
+    var navigateMode: RouterNavigateMode
+    
+    var viewController: UIViewController
+    
+    var dataProvider: DataProvider
+    
+    public init() {
+        navigateMode = .push
+        viewController = ViewController()
+        dataProvider = DemoDataProvider()
+    }
+    
+    func componentWillActive() {
+        print(router?.mainUrl.description)
+    }
+}
