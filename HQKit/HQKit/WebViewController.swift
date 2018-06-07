4f8832401ab91646bf8876ee8c4ee90952570320
Magee Huang <magee.huang@blackboard.com>
2018-06-07 16:56:57 +0800

    Add webcontroller

---
+++ HQKit/HQKit/WebViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WebViewController.swift
+//  HQKit
+//
+//  Created by Magee Huang on 6/7/18.
+//  Copyright © 2018 com.personal.HQ. All rights reserved.
+//
+
+import UIKit
+
+open class WebViewController: UIViewController {
+    var request: URLRequest?
+    var navBar: UIView?
+    var toolBar: UIView?
+    var progressView: UIView?
+    var optionsStackView: UIStackView?
+    var transparentView: UIView?
+    
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
