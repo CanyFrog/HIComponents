186089205b6f708488acb21d940651cc4183b145
honqi <honqi3014@gmail.com>
2018-06-11 14:16:00 +0800

    WebViewController

---
+++ HQKit/HQKitDemo/Controllers/WebViewDemo.swift
@@ -0,0 +1,25 @@
+//
+//  WebViewDemo.swift
+//  HQKitDemo
+//
+//  Created by Magee Huang on 6/11/18.
+//  Copyright © 2018 com.personal.HQ. All rights reserved.
+//
+
+import HQKit
+
+class WebViewDemo: BaseDemo {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let BaiduBtn = initBtn(title: "BAIDU")
+        BaiduBtn.hq.addEvent({
+            let web = WebViewController(url: "https://www.baidu.com")
+            web.title = self.title
+            self.present(web, animated: true, completion: nil)
+        }, .touchUpInside)
+        
+        BaiduBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        BaiduBtn.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
