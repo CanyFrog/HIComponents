df9bfebbec13917b316377292e8196f9c79f13df
Magee Huang <magee.huang@blackboard.com>
2018-06-12 17:09:25 +0800

    。。。。

---
+++ HQDownload/HQDownload/Progress+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Progress+Extension.swift
+//  HQDownload
+//
+//  Created by Magee Huang on 6/12/18.
+//  Copyright © 2018 com.personal.HQ. All rights reserved.
+//
+
+import HQFoundation
+
+extension Progress {
+    
+}
+extension Namespace where T: Progress {
+//    var fileURL
+}
