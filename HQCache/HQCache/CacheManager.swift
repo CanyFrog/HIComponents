d9ff98117de902b8073e01230d73f5a94898a86c
honqi <honqi3014@gmail.com>
2018-07-26 17:07:58 +0800

    Remove and change api

--- HQCache/HQCache/CacheManager.swift
+++ HQCache/HQCache/CacheManager.swift
@@ -9,98 +9,58 @@
 import Foundation
 
 
-public final class CacheManager {
-    public private(set) var memoryCache: MemoryCache!
-    public private(set) var diskCache: DiskCache!
-    
+public final class Cache {
+    public private(set) var memory: MemoryCache!
+    public private(set) var disk: DiskCache!
+
     public init(_ path: URL) {
-        diskCache = DiskCache(path)
-        if diskCache == nil { fatalError("Path is invalid") }
-        memoryCache = MemoryCache()
-        
+        disk = DiskCache(path)
+        if disk == nil { fatalError("Path is invalid") }
+        memory = MemoryCache()
     }
-    public convenience init(_ name: String = "cacheManager") {
+    
+    public convenience init(_ name: String = "Cache") {
         self.init(URL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first!).appendingPathComponent(name))
     }
 }
 
-extension CacheManager {
-    public func exist(forKey key: String) -> Bool {
-        return memoryCache.exist(forKey:key) || diskCache.exist(forKey:key)
+extension Cache {
+    public func contains(_ key: String) -> Bool {
+        return memory.contains(key) || disk.contains(key)
     }
-    
-    public func exist(forKey key: String, inBackThreadCallback callback: @escaping (String, Bool) -> Void) {
-        if memoryCache.exist(forKey: key) {
-            DispatchQueue.global(qos: .default).async {
-                callback(key, true)
-            }
-        }
-        else {
-            diskCache.exist(forKey: key, inBackThreadCallback: callback)
-        }
-    }
-    
-    public func query<T: Codable>(objectForKey key: String) -> T? {
-        var obj: T? = memoryCache.query(objectForKey: key)
+
+    public func object<T: Codable>(forKey key: String) -> T? {
+        var obj: T? = memory.object(forKey: key)
         if obj == nil {
-            obj = diskCache.query(objectForKey: key)
+            obj = disk.object(forKey: key)
             if obj != nil {
-                memoryCache.insertOrUpdate(object: obj, forKey: key)
+                memory.setObject(obj, forKey: key)
             }
         }
         return obj
     }
-    
-    public func query<T: Codable>(objectForKey key: String, inBackThreadCallback callback: @escaping (String, T?) -> Void) {
-        if let obj: T? = memoryCache.query(objectForKey: key) {
-            DispatchQueue.global(qos: .default).async {
-                callback(key, obj)
-            }
-        }
-        else {
-            diskCache.query(objectForKey: key) { (k: String, v: T?) in
-                if v != nil && !self.memoryCache.exist(forKey: key) {
-                    self.memoryCache.insertOrUpdate(object: v, forKey: k)
-                }
-                callback(k,v)
-            }
-        }
+
+
+    public func setObject<T: Codable>(_ obj: T, forKey key: String) {
+        memory.setObject(obj, forKey: key)
+        disk.setObject(obj, forKey: key)
     }
-    
-    
-    public func insert<T: Codable>(object obj: T, forKey key: String) {
-        memoryCache.insertOrUpdate(object: obj, forKey: key)
-        diskCache.insertOrUpdate(object: obj, forKey: key)
+
+
+    public func removeObject(forKey key: String) {
+        memory.removeObject(forKey: key)
+        disk.removeObject(forKey: key)
     }
+
     
-    public func insertOrUpdate<T: Codable>(object obj: T, forKey key: String, inBackThreadCallback callback: @escaping () -> Void) {
-        memoryCache.insertOrUpdate(object: obj, forKey: key)
-        diskCache.insertOrUpdate(object: obj, forKey: key, inBackThreadCallback: callback)
+    public func removeAllObjects() {
+        memory.removeAllObjects()
+        disk.removeAllObjects()
     }
-    
-    
-    public func delete(objectForKey key: String) {
-        memoryCache.delete(objectForKey: key)
-        diskCache.delete(objectForKey: key)
-    }
-    
-    public func delete(objectForKey key: String, inBackThreadCallback callback: @escaping (String) -> Void) {
-        memoryCache.delete(objectForKey: key)
-        diskCache.delete(objectForKey: key, inBackThreadCallback: callback)
-    }
-    
-    public func deleteAllCache() {
-        memoryCache.deleteAllCache()
-        diskCache.deleteAllCache()
-    }
-    
-    public func deleteAllCache(inBackThread callback: @escaping () -> Void) {
-        memoryCache.deleteAllCache()
-        diskCache.deleteAllCache(inBackThread: callback)
-    }
-    
-    public func deleteAllCache(withProgressClosure progress: @escaping (Int, Int, Bool) -> Void) {
-        memoryCache.deleteAllCache()
-        diskCache.deleteAllCache(withProgressClosure: progress)
+
+
+    public func removeAllObjects(progress: @escaping (Int, Int, Bool) -> Void) {
+        memory.removeAllObjects()
+        disk.removeAllObjects(progress: progress)
     }
 }
