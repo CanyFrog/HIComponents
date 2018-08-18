00b906234d7d10944840e4dbfd1e05ca6eb7ef92
HonQii <honqi@HonQi-iMac.local>
2018-08-18 12:19:00 +0800

    Refined

---
+++ HQXMLDoc/HQXMLDoc/DOC.swift
@@ -0,0 +1,111 @@
+//
+//  DOC.swift
+//  HQXMLDoc
+//
+//  Created by HonQi on 8/16/18.
+//  Copyright © 2018 HonQi Indie. All rights reserved.
+//
+
+import libxml2
+
+public class DOC {
+    let docPtr: xmlDocPtr
+
+    public let rootNode: Node
+    
+    public var version: String? {
+        if let ver = docPtr.pointee.version { return String(cString: ver) }
+        return nil
+    }
+    
+    public var encoding: String.Encoding? {
+        guard let encode = docPtr.pointee.encoding else { return nil }
+        let encodeName = String(cString: encode)
+        let encodeCode = CFStringConvertIANACharSetNameToEncoding(encodeName as CFString)
+        guard encodeCode != kCFStringEncodingInvalidId else { return nil }
+        return String.Encoding(rawValue: CFStringConvertEncodingToNSStringEncoding(encodeCode))
+    }
+    
+    public convenience init(xml data: Data, encoding: String.Encoding? = nil) throws {
+        try self.init(xml: true, data: data, encoding: encoding)
+    }
+    
+    public convenience init(html data: Data, encoding: String.Encoding? = nil) throws {
+        try self.init(xml: false, data: data, encoding: encoding)
+    }
+    
+    public convenience init(html str: String, encoding: String.Encoding = .utf8) throws {
+        guard let data = str.data(using: encoding) else { throw DocError.dataEmpty }
+        try self.init(html: data, encoding: encoding)
+    }
+    
+    public convenience init(xml str: String, encoding: String.Encoding = .utf8) throws {
+        guard let data = str.data(using: encoding) else { throw DocError.dataEmpty }
+        try self.init(xml: data, encoding: encoding)
+    }
+    
+    init(xml: Bool, data: Data, encoding: String.Encoding? = nil) throws {
+        guard !data.isEmpty else { throw DocError.dataEmpty }
+        
+        var encode: UnsafePointer<Int8>? = nil
+        if let e = encoding {
+            let cfEncoding = CFStringConvertNSStringEncodingToEncoding(e.rawValue)
+            let cfEncodingAsString = CFStringConvertEncodingToIANACharSetName(cfEncoding)
+            encode = CFStringGetCStringPtr(cfEncodingAsString, 0)
+        }
+        
+        let options: Int32 = 1 << 0 | 1 << 5 | 1 << 6
+        
+        let cDatas = data.withUnsafeBytes { (bytes: UnsafePointer<Int8>) -> [CChar] in
+            return [CChar](UnsafeBufferPointer(start: bytes, count: data.count))
+        }
+        
+        var DOCPtr: xmlDocPtr!
+        if xml {
+            DOCPtr = xmlReadMemory(cDatas, Int32(data.count), nil, encode, options)
+        }
+        else {
+            DOCPtr = htmlReadMemory(cDatas, Int32(data.count), nil, encode, options)
+        }
+        
+        guard let ptr = DOCPtr else {
+            if let err = xmlGetLastError() {
+                defer { xmlResetLastError() }
+                throw DocError.XMLError(err.pointee.code, String(cString: err.pointee.message).trimmingCharacters(in: CharacterSet.whitespacesAndNewlines))
+            }
+            throw DocError.Unknown
+        }
+        
+        docPtr = ptr
+        guard let root = Node(nodePtr: xmlDocGetRootElement(ptr)) else { throw DocError.missingRootNode }
+        rootNode = root
+    }
+    
+    deinit {
+        xmlFreeDoc(docPtr)
+    }
+}
+
+extension DOC {
+    public func xpath(_ path: String) -> [Node]? {
+        return rootNode.xpath(path)
+    }
+}
+
+extension DOC: CustomStringConvertible {
+    public var description: String {
+        return rootNode.description
+    }
+}
+
+extension DOC: Equatable {
+    public static func == (lhs: DOC, rhs: DOC) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+}
+
+extension DOC: Hashable {
+    public var hashValue: Int {
+        return docPtr.hashValue
+    }
+}
---
+++ HQXMLDoc/HQXMLDoc/Node.swift
@@ -0,0 +1,172 @@
+//
+//  Node.swift
+//  HQXMLDoc
+//
+//  Created by HonQi on 8/16/18.
+//  Copyright © 2018 HonQi Indie. All rights reserved.
+//
+
+import libxml2
+
+public class Node {
+    var nodePtr: xmlNodePtr!
+    
+    /// common
+    public var name: String { return String(cString: nodePtr.pointee.name) }
+    public var lineNumber: Int { return Int(xmlGetLineNo(nodePtr)) }
+    public lazy var isBlank = self.content.count == 0
+    
+    
+    /// content
+    /// contains all child string
+    public var content: String {
+        guard let c = xmlNodeGetContent(self.nodePtr) else { return "" }
+        defer { xmlFree(c) }; return String(cString: c)
+    }
+    
+    public var rawStringValue: String? {
+        guard let content = nodePtr.pointee.content else { return nil }
+        return String(cString: content)
+    }
+    
+    public var stringValue: String? { return rawStringValue?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) }
+    
+    /// contains tag name
+    public var htmlString: String? {
+        guard let buffer = xmlBufferCreate() else { return nil }
+        xmlNodeDump(buffer, nodePtr.pointee.doc, nodePtr, 0, 0)
+        defer { xmlBufferFree(buffer) }
+        return String(cString: xmlBufferContent(buffer))
+    }
+    
+    
+    /// relationship chain
+    public var parent: Node? { return Node(nodePtr: nodePtr.pointee.parent) }
+    public var prevSibling: Node? { return Node(nodePtr: nodePtr.pointee.prev) }
+    public var nextSibling: Node? { return Node(nodePtr: nodePtr.pointee.next) }
+    public var firstChild: Node? { return Node(nodePtr: nodePtr.pointee.children) }
+    public var lastChild: Node? { return Node(nodePtr: nodePtr.pointee.last) }
+    public var childCount: Int { return Int(xmlChildElementCount(nodePtr)) }
+    public var children: [Node] {
+        var arr = [Node]()
+        var child = nodePtr.pointee.children
+        while let c = child, xmlNodeIsText(c) == 0 {
+            if let n = Node(nodePtr: c) {
+                arr.append(n)
+            }
+            child = c.pointee.next
+        }
+        return arr
+    }
+    
+    public func child(at idx: Int) -> Node? {
+        let arr = children
+        return idx < arr.count ? arr[idx] : nil
+    }
+
+    
+    /// attribute
+    public var attributes: [String: String] {
+        var dicts = [String: String]()
+        var props = nodePtr.pointee.properties
+        while let p = props {
+            if let c = xmlGetProp(nodePtr, p.pointee.name) {
+                dicts[String(cString: p.pointee.name)] = String(cString: c)
+                xmlFree(c)
+            }
+            props = p.pointee.next
+        }
+        return dicts
+    }
+    
+    public func attribute(for name: String) -> String? {
+        let xmlstr = name.utf8CString.map { xmlChar(bitPattern: $0) }
+        return xmlstr.withUnsafeBufferPointer {
+            if let attrValue = xmlGetProp(nodePtr, $0.baseAddress!) {
+                let value = String(cString: attrValue)
+                xmlFree(attrValue)
+                return value
+            }
+            return nil
+        }
+    }
+    
+    
+    /// type
+    public var isAttributeNode: Bool { return nodePtr.pointee.type == xmlElementType(rawValue: 2) }
+    public var isDocumentNode: Bool { return nodePtr.pointee.type == xmlElementType(rawValue: 13) }
+    public var isElementNode: Bool { return nodePtr.pointee.type == xmlElementType(rawValue: 1) }
+    public var isTextNode: Bool { return nodePtr.pointee.type == xmlElementType(rawValue: 3) }
+    
+    
+    init?(nodePtr: xmlNodePtr!) {
+        guard let ptr = nodePtr else { return }
+        self.nodePtr = ptr
+    }
+    
+    deinit {
+        xmlFreeNode(nodePtr)
+    }
+}
+
+extension Node {
+    public func xpath(_ path: String) -> [Node]? {
+        guard let data = path.data(using: .utf8, allowLossyConversion: false) else { return nil }
+
+        guard let context = xmlXPathNewContext(nodePtr.pointee.doc) else { return nil }
+        defer { xmlXPathFreeContext(context) }
+        context.pointee.node = nodePtr
+        
+        // Due to a bug in libxml2, namespaces may not appear in `xmlNode->ns`.
+        // As a workaround, `xmlNode->nsDef` is recursed to explicitly register namespaces.
+        var node = nodePtr
+        while let p = node?.pointee.parent {
+            var ns = node?.pointee.nsDef
+            while let n = ns  {
+                if let pf = n.pointee.prefix { xmlXPathRegisterNs(context, pf, n.pointee.href) }
+                ns = n.pointee.next
+            }
+            node = p
+        }
+        
+        let cDatas = data.withUnsafeBytes { return [xmlChar](UnsafeBufferPointer(start: $0, count: data.count)) }
+        
+        guard let xpathPtr = xmlXPathEvalExpression(cDatas, context) else { return nil }
+        defer { xmlXPathFreeObject(xpathPtr) }
+
+        guard let val = xpathPtr.pointee.nodesetval,
+            let tab = val.pointee.nodeTab else { return nil }
+        
+        var nodes = [Node]()
+        for idx in 0 ..< Int(val.pointee.nodeNr) {
+            if let ptr = tab[idx], let n = Node(nodePtr: ptr) {
+                nodes.append(n)
+            }
+        }
+        return nodes
+    }
+}
+
+
+extension Node: CustomStringConvertible {
+    public var description: String { return htmlString ?? "" }
+}
+
+extension Node: Equatable {
+    public static func == (lhs: Node, rhs: Node) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+}
+
+extension Node: Hashable {
+    public var hashValue: Int {
+        return nodePtr.hashValue
+    }
+}
+
+extension Node: IteratorProtocol {
+    public typealias Element = Node
+    public func next() -> Node? {
+        return nextSibling
+    }
+}
