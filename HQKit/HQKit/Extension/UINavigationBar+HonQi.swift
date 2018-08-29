30f71824c4218eb289ff95c593f330c19f9dfb8b
honqi <honqi3014@gmail.com>
2018-08-29 15:46:55 +0800

    Add navigation extension

--- HQKit/HQKit/Extension/UINavigationBar+HonQi.swift
+++ HQKit/HQKit/Extension/UINavigationBar+HonQi.swift
@@ -8,36 +8,36 @@
 
 import HQFoundation
 
+var NavigationBarMargin: UInt8 = 001
 extension Namespace where T: UINavigationBar {
     /// Set back button image, only change image
-    public var backButtonImage: UIImage? {
-        get {
-            return instance.backIndicatorImage
-        }
-        set {
-            let image: UIImage? = newValue
-            instance.backIndicatorImage = image
-            instance.backIndicatorTransitionMaskImage = image
+    public var backButtonImage: UIImage? { return instance.backIndicatorImage }
+
+    public func set(backButtonImage: UIImage?) {
+        instance.backIndicatorImage = backButtonImage
+        instance.backIndicatorTransitionMaskImage = backButtonImage
+    }
+    
+    /// Set backgroud iamge with white color and alpha
+    public func set(backAlpha: CGFloat) {
+        instance.setBackgroundImage(UIImage.hq.image(color: UIColor.white.withAlphaComponent(backAlpha), size: UIScreen.main.bounds.size), for: .default)
+    }
+    
+    /// Hidden navigation bar bottom line
+    public func hiddenLine() {
+        instance.shadowImage = UIImage()
+        if instance.backgroundImage(for: .default) == nil {
+            instance.setBackgroundImage(UIImage(), for: .default)
         }
     }
 }
 
-var NavigationBarMargin: UInt8 = 001
-extension UINavigationBar {
-    /// Adjust iOS 11.0 ~ Bar margin; default is 8px
-    public var hq_itemMargin: CGFloat? {
-        get { return hq.associatedObject(key: &NavigationBarMargin)}
-        set {
-            hq.setAssociateObject(key: &NavigationBarMargin, value: hq_itemMargin, policy: objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
-            setNeedsLayout()
-        }
-    }
-    
-    open override func layoutSubviews() {
-        super.layoutSubviews()
-        if #available(iOS 11.0, *) {
-            let margin = hq_itemMargin ?? 0
-            subviews.forEach{ if NSStringFromClass($0.classForCoder).contains("ContentView") { $0.layoutMargins = UIEdgeInsets(top: 0, left: margin, bottom: 0, right: margin) } }
-        }
-    }
-}
+//extension UINavigationBar {
+//    open override func layoutSubviews() {
+//        super.layoutSubviews()
+//        if #available(iOS 11.0, *) {
+//            subviews.forEach{ if NSStringFromClass($0.classForCoder).contains("ContentView") { $0.layoutMargins = UIEdgeInsets.zero }
+//            }
+//        }
+//    }
+//}
