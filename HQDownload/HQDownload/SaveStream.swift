0985887b1249bb9bd640229d48b88385dfa0b74d
honqi <honqi3014@gmail.com>
2018-11-14 11:24:23 +0800

    add podfile

---
+++ HQDownload/HQDownload/SaveStream.swift
@@ -0,0 +1,13 @@
+//
+//  SaveStream.swift
+//  HQDownload
+//
+//  Created by Magee Huang on 11/14/18.
+//  Copyright © 2018 HonQi Indie. All rights reserved.
+//
+
+import UIKit
+
+class SaveStream: OutputStream {
+
+}
